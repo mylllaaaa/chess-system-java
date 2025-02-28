e394eed36d5d109c404d66d768ef6f0afa285943
Myllena Medeiros <myllenamed19@gmail.com>
2025-02-28 08:57:44 -0300

    Possible moves of a piece

--- src/board/game/Piece.java
+++ src/board/game/Piece.java
@@ -1,6 +1,6 @@
 package board.game;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position; //posição de matriz - não será vizível na camada Chess
 	private Board board;
@@ -13,5 +13,24 @@ public class Piece {
 	protected Board getBoard() { //apenas classes dentro do mesmo pacote e subclasses podem acessar o tabuleiro de uma peça 
 		return board;
 	}
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
 		
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import board.game.Board;
 import board.game.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 	
 	private Color color;
 
